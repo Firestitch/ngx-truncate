3eadd6eef3bf7a12a924c1a8f1fec6953191bc58
Raymond <ray@firestitch.com>
2018-01-26 13:04:38 -0500

    renamed input-single

---
+++ playground/app/components/example-component/example-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'example-component',
+  templateUrl: 'example-component.component.html'
+})
+export class ExampleComponentComponent {
+
+  x = 'bob';
+}
--- playground/playground.module.ts
+++ playground/playground.module.ts
@@ -8,7 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app/material.module';
 import { FsComponentExampleComponent } from './app/components/fs-component-example/fs-component-example.component';
 import { FsComponentExamplesComponent } from './app/components/fs-component-examples/fs-component-examples.component';
-import { InputSingleComponent } from './app/components/input-single/input-single.component';
+import { ExampleComponentComponent } from './app/components/example-component/example-component.component';
 
 @NgModule({
   bootstrap: [ AppComponent ],
@@ -24,7 +24,7 @@ import { InputSingleComponent } from './app/components/input-single/input-single
     AppComponent,
     FsComponentExampleComponent,
     FsComponentExamplesComponent,
-    InputSingleComponent
+    ExampleComponentComponent
   ],
   providers: [
   ],
