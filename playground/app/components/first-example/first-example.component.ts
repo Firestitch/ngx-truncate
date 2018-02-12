214fe8304b7b23e630e01da44aef776cec7acdd4
Raymond <ray@firestitch.com>
2018-02-12 16:31:34 -0500

    added compdoc

--- playground/app/components/first-example/first-example.component.ts
+++ playground/app/components/first-example/first-example.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'first-example',
-  templateUrl: 'first-example.component.html',
-  styleUrls: [ 'first-example.component.css' ]
+  templateUrl: 'first-example.component.html'
 })
 export class FirstExampleComponent {
 }
