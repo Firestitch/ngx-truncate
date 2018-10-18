37362709e1a1d962990a9adfd2d5b007002c1353
Eugene Kubesh <e.kubesh@wis.eu.com>
2018-10-18 18:35:44 +0300

    upgraded to ng-cli

--- src/app/fs-component.module.ts
+++ src/app/fs-component.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { FsComponentComponent } from './components/fs-component/fs-component.component';
 // import { FsComponentService } from './services';
 
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './fs-component.module';
-export * from './components';
-// export * from './services';
-// export * from './models';
-// export * from './interfaces';
-
