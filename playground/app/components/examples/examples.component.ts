ff95bf2e176a5b0ea2865f5b7ba4b62db2385dfe
Raymond Gigliotti <ray@firestitch.com>
2019-01-21 13:21:11 -0500

    added base-environment.ts

--- playground/app/components/examples/examples.component.ts
+++ playground/app/components/examples/examples.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   templateUrl: 'examples.component.html'
 })
 export class ExamplesComponent {
+  public config = environment;
 }
