648c76d7f9da468cf4ee7ebe65d669ed6bd2edf3
Raymond Gigliotti <ray@firestitch.com>
2019-03-01 11:28:10 -0500

    7.0.0 title to name rename

--- playground/app/components/example/example.component.ts
+++ playground/app/components/example/example.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'example',
-  templateUrl: 'example.component.html'
+  templateUrl: 'example.component.html',
+  styleUrls: ['example.component.scss']
 })
 export class ExampleComponent {
 }
