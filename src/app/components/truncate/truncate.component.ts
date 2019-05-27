f65cbe65cd4b57657b6b3a9c103240387a842f5f
Raymond Gigliotti <ray@firestitch.com>
2019-05-27 15:23:57 -0400

    Used MutationObserver to detect changes in the content size to determine tooltip / more

--- src/app/components/truncate/truncate.component.ts
+++ src/app/components/truncate/truncate.component.ts
@@ -1,16 +1,20 @@
 import {  Component, Input, ViewChild, ElementRef, AfterContentInit,
-          AfterContentChecked, EventEmitter, Output, OnInit } from '@angular/core';
+          EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 
 @Component({
   selector: 'fs-truncate',
   templateUrl: 'truncate.component.html',
   styleUrls: [ 'truncate.component.scss' ],
 })
-export class FsTruncateComponent implements AfterContentInit, AfterContentChecked, OnInit {
+export class FsTruncateComponent implements AfterContentInit, OnDestroy, OnInit {
 
   @ViewChild('contentEl') contentEl: ElementRef;
+  @ViewChild('contentWrapper') contentWrapper: ElementRef;
 
-  @Output() public readonly contentChanged = new EventEmitter<string>();
+  @Output() public readonly contentChanged = new EventEmitter();
 
   @Input() lines = 1;
   @Input() more = false;
@@ -20,41 +24,58 @@ export class FsTruncateComponent implements AfterContentInit, AfterContentChecke
   public truncate = true;
   public truncated = false;
 
+  private _mutationObserver: MutationObserver;
+  private _destroy$ = new Subject();
+
   moreClick() {
     this.truncate = false;
   }
 
   lessClick() {
     this.truncate = true;
-    this.calculateTruncated();
+    setTimeout(() => {
+      this.calculateTruncated();
+    });
   }
 
   calculateTruncated() {
     if (this.truncate) {
-      setTimeout(() => {
-        const element = this.contentEl.nativeElement;
-        this.truncated = element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
-      });
+      const element = this.contentEl.nativeElement;
+
+      // Sometimes because of line-height and font-size % there could could be a difference in height
+      this.truncated = (element.scrollHeight / element.clientHeight) >= 1.2;
     }
   }
 
   ngOnInit() {
     this.contentChanged
+    .pipe(
+      takeUntil(this._destroy$)
+    )
     .subscribe(() => {
+      this.content = this.contentEl.nativeElement.innerHTML;
       this.calculateTruncated();
     });
   }
 
   ngAfterContentInit(): void {
-    this.content = this.contentEl.nativeElement.innerHTML;
-    this.contentChanged.emit(this.content);
+
+    this.contentChanged.emit();
+
+    this._mutationObserver = new MutationObserver((x, y) => {
+      this.contentChanged.emit();
+    });
+
+    const config = { attributes: false, childList: true, characterData: true, subtree: true };
+    this._mutationObserver.observe(this.contentEl.nativeElement, config);
   }
 
-  ngAfterContentChecked(): void {
-    const innerHTML = this.contentEl.nativeElement.innerHTML;
-    if (innerHTML !== this.content) {
-        this.content = innerHTML;
-        this.contentChanged.emit(this.content);
+  ngOnDestroy(): void {
+    if (this._mutationObserver) {
+      this._mutationObserver.disconnect();
     }
+
+    this._destroy$.next();
+    this._destroy$.complete();
   }
 }
