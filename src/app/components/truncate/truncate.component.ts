cfb08d72ecbe700dd804802dcb11eea2c1c241a0
Eugene Kubesh <e.kubesh@wis.eu.com>
2019-09-14 18:23:32 +0300

    fixed performance issues and upgraded to 7.0.7

--- src/app/components/truncate/truncate.component.ts
+++ src/app/components/truncate/truncate.component.ts
@@ -1,15 +1,28 @@
-import {  Component, Input, ViewChild, ElementRef, AfterContentInit,
-          EventEmitter, Output, OnInit, OnDestroy, HostBinding, NgZone, ApplicationRef, ɵConsole } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostBinding,
+  Input,
+  NgZone,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 
 @Component({
   selector: 'fs-truncate',
   templateUrl: 'truncate.component.html',
-  styleUrls: [ 'truncate.component.scss' ],
+  styleUrls: ['truncate.component.scss'],
 })
-export class FsTruncateComponent implements AfterContentInit, OnDestroy, OnInit {
+export class FsTruncateComponent implements OnInit, AfterContentInit, OnChanges, OnDestroy {
 
   @ViewChild('contentEl') contentEl: ElementRef;
   @ViewChild('contentWrapper') contentWrapper: ElementRef;
@@ -19,63 +32,102 @@ export class FsTruncateComponent implements AfterContentInit, OnDestroy, OnInit 
   @Output() public readonly contentChanged = new EventEmitter();
 
   @Input() lines = 1;
-  @Input() more = false;
+  @Input('more') moreVisible = false;
   @Input() tooltip = true;
 
   public content = '';
   public truncate = true;
+  public linesClass = '';
+
+  public moreBtnVisible = false;
+  public lessBtnVisible = false;
+
+  public tooltipDisabled = true;
 
   private _mutationObserver: MutationObserver;
   private _destroy$ = new Subject();
 
-  constructor() {}
+  constructor(private _zone: NgZone) {}
 
-  moreClick() {
-    this.truncate = false;
-  }
-
-  lessClick() {
-    this.truncate = true;
-    setTimeout(() => {
-      this.calculateTruncated();
-    });
-  }
-
-  calculateTruncated() {
-    const element = this.contentEl.nativeElement;
-    // Sometimes because of line-height and font-size % there could could be a difference in height
-    this.truncated = (element.scrollHeight / element.clientHeight) >= 1.2;
-  }
-
-  ngOnInit() {
+  public ngOnInit() {
     this.contentChanged
-    .pipe(
-      takeUntil(this._destroy$)
-    )
-    .subscribe(() => {
-      this.content = this.contentEl.nativeElement.innerHTML;
-      this.calculateTruncated();
+      .pipe(
+        debounceTime(200),
+        takeUntil(this._destroy$),
+      )
+      .subscribe(() => {
+        this.content = this.contentEl.nativeElement.innerHTML;
+        this._calculateTruncated();
+      });
+  }
+
+  public ngAfterContentInit(): void {
+    this._zone.runOutsideAngular(() => {
+      this._mutationObserver = new MutationObserver(() => {
+        this.contentChanged.emit();
+      });
+
+      const config = { attributes: false, childList: true, characterData: true, subtree: true };
+      this._mutationObserver.observe(this.contentEl.nativeElement, config);
     });
   }
 
-  ngAfterContentInit(): void {
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes) {
+      if (changes.moreVisible || changes.tooltip) {
+        this._checkButtonsVisibility();
+        this._checkTooltipDisabled();
+      }
 
-    this.contentChanged.emit();
-
-    this._mutationObserver = new MutationObserver(() => {
-      this.contentChanged.emit();
-    });
-
-    const config = { attributes: false, childList: true, characterData: true, subtree: true };
-    this._mutationObserver.observe(this.contentEl.nativeElement, config);
+      if (changes.lines) {
+        this.linesClass = 'lines-' + this.lines;
+      }
+    }
   }
 
-  ngOnDestroy(): void {
+  public ngOnDestroy(): void {
     if (this._mutationObserver) {
       this._mutationObserver.disconnect();
     }
 
     this._destroy$.next();
     this._destroy$.complete();
+  }
+
+  public moreClick() {
+    this.truncate = false;
+    this._checkButtonsVisibility();
+  }
+
+  public lessClick() {
+    this.truncate = true;
+    this._checkButtonsVisibility();
+    setTimeout(() => {
+      this._calculateTruncated();
+    });
+  }
+
+  private _calculateTruncated() {
+    const element = this.contentEl.nativeElement;
+    // Sometimes because of line-height and font-size % there could could be a difference in height
+    this.truncated = (element.scrollHeight / element.clientHeight) >= 1.2;
+
+    this._checkTooltipDisabled();
+  }
+
+  private _checkButtonsVisibility() {
+    if (this.moreVisible) {
+      if (this.truncate) {
+        this.moreBtnVisible = true;
+        this.lessBtnVisible = false;
+      } else {
+        this.moreBtnVisible = false;
+        this.lessBtnVisible = true;
+      }
+    }
+  }
+
+  private _checkTooltipDisabled() {
+    this.tooltipDisabled = !this.truncated || !this.tooltip || this.moreVisible;
   }
 }
